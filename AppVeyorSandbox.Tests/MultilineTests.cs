5aaf9df555c4646856292319918a3696fa881798
Cameron MacFarland <distantcam@gmail.com>
2014-12-10 14:52:32 +0800

    Added simple multiline test

---
+++ AppVeyorSandbox.Tests/MultilineTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using Xunit;
+
+namespace AppVeyorSandbox.Tests
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class MultilineTests
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        [Fact]
+        public void Multiline1()
+        {
+            Approvals.Verify(@"This
+Is
+A
+Simple
+Multiline
+String
+");
+        }
+    }
+}
